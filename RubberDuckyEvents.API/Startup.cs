95bb73379bef041781392e14e1d6f4efc46dcd5b
Bailey Lievens <baileylievens@hotmail.be>
2021-11-05 12:08:04 +0100

    GetByID works

--- RubberDuckyEvents.API/Startup.cs
+++ RubberDuckyEvents.API/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using RubberDuckyEvents.API.Infra;
+using RubberDuckyEvents.API.Ports;
 
 namespace RubberDuckyEvents.API
 {
@@ -24,12 +19,13 @@ namespace RubberDuckyEvents.API
         }
 
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<RubberDuckyContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));        
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IDatabase, SqliteDatabase>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
